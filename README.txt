4ba5c7b0536185c6ea3cb21b08c929988cfd1864
Chris Lindgren <chris.a.lindgren@gmail.com>
2014-03-28 08:43:16 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+This Chrome extension marks all text on a page as 'undefined.' It is a (quirky) response to @DanielleSucher's Jailbreak the Patriarchy (JTP) extension.
+
+Rather than swap gendered pronouns, as does JTP, this extension exploits the way javascript and the web browser deal with the empty associative arrays. Since the arrays are all declared, but empty, they are interpreted as "undefined." So, instead of replacing any words with the regex .replace() method, it graciously appends "undefined" before every single word within the <body> of the page.
+
+To be honest, I think this iteration of the original extension is rather absurd. This code is a really bad, brute-force, blank-slate argument about the relationship between langauge and the world. In short, it's quite the contradiction to label *everything* "undefined." I chose to post the source here, because I happened to break the extension in an interesting way, while tinkering around with it. 
+
+My main goal is to write an iteration that adds what I envision as an interrogation layer. I plan on using regex in a similar fashion to locate the gendered pronouns for users, but, instead of using the regex .replace() method, I will use the .test() method to provide users with a variety of options to learn more about the relationships between language, bodies, gender, and identity. Particularly when such relationships concern the too often neglected narratives of people who identify as non-binary and transgender. Some of my initial URI data plans are mapped out here: http://blog.clindgrencv.com/post/73526415972/web-app-uri-data-design-ideas-part-i
+
+***
+
+Original extension, Jailbreak the Patriarchy, by @DanielleSucher.
+More information about Sucher's original extension:
+http://www.daniellesucher.com/2011/11/jailbreak-the-patriarchy-my-first-chrome-extension/
---
+++ myscript.js
@@ -0,0 +1,81 @@
+/* =================================================
+    'Undefined' by Chris Lindgren
+    Code response to/iteration of @DanielleSucher's 
+    'Jailbreak the Patriarchy' web browser extension 
+    for Chrome.
+
+    To run it in Chrome:
+    1. Download it.
+    2. Open Chrome and goto: chrome://extensions/
+    3. Click "Load unpacked extension"
+    4. Navigate/find the folder
+    5. Click "Open" to unpack
+==================================================*/
+var simplyNothing = { };
+var moreNothing = { };
+var stillNothing = { };
+var enoughNothing = { };
+
+var map = {};
+var maps = [ simplyNothing, moreNothing, stillNothing, enoughNothing ];
+for (var i = 0; i < maps.length; i++) {
+  for (attr in maps[i]) {
+    map[attr] = maps[i][attr];
+  }
+}
+
+var concatNothing = function(obj) {
+  var parts = [];
+  for (key in obj) {
+    parts.push(key);
+  }
+  return parts.join('|');
+};
+
+var regex = '^('
+              + concatNothing(simplyNothing)
+              + ')|('
+              + concatNothing(moreNothing)
+              + ')$|('
+              + concatNothing(stillNothing)
+              + ')|^('
+              + concatNothing(enoughNothing)
+              + ')$';
+
+var searchForNothing = new RegExp(regex, 'i');
+
+function findUndefined(word) {
+  return map[word];
+}
+
+function swapNothing(word) {
+  return word
+    .replace(searchForNothing, findUndefined)
+}
+
+function nothingSwap(text) {
+  return text
+    .replace(/\b([a-z][\w']+)\b/gi, swapNothing)
+}
+
+function jailbreakNothing(node){
+  var treeWalker = document.createTreeWalker(
+      node,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+  );
+  while(treeWalker.nextNode()) {
+    var current = treeWalker.currentNode;
+    current.textContent = nothingSwap(current.textContent);
+  }
+}
+
+chrome.extension.sendRequest( { name: "isPaused?" }, function(response) {
+  if (response.value != 'true') {
+    jailbreakNothing(document.body);
+    document.body.addEventListener('DOMNodeInserted', function(event) {
+      jailbreakNothing(event.target);
+    });
+  }
+});
